e4f1aa5d903e691cedf12381ec5d3a7327b0281b
joshuacronemeyer <joshuacronemeyer@0249d32c-82a8-11dd-9299-37fd5bf03096>
2008-09-21 21:41:06 +0000

    Feature Complete!!!  Just fixed the high score display so grudge matches show the winners and losers correctly

---
+++ trunk/test_machine.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'test/unit'
+
+class TestMachine < Test::Unit::TestCase
+  def test_is_grudge_match_machine
+    Database.test_connect
+    Score.add_high_score("js","js",23)
+    assert(!Machine.find_by_name("js").is_grudge_match_machine?)
+    GrudgeMatch.add_score("js","ps","Galaga")
+    assert(Machine.find_by_name("Galaga").is_grudge_match_machine?)
+    Database.disconnect
+  end
+end
