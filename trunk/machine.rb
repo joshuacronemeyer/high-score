bef60e0bd157939c63519ded3397141f460b63cf
warren.greg <warren.greg@0249d32c-82a8-11dd-9299-37fd5bf03096>
2008-11-15 18:55:30 +0000

    machine and player score collections get highest scores only.  Sort players display by machine

--- trunk/machine.rb
+++ trunk/machine.rb
@@ -1,7 +1,12 @@
 require 'active_record'
 
 class Machine < ActiveRecord::Base
-  has_many :score
+  has_many :score, :finder_sql =>
+    'select id, machine_id, player_id, max(score) as score ' +
+    'from scores ' +
+    'where scores.machine_id = #{id} ' +
+    'group by machine_id, player_id'
+    
   has_many :grudge_match
 
   def is_grudge_match_machine?
