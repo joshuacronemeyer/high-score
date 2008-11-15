9775a87d8bdaed3754d1a188451ec2015ed18eb4
warren.greg <warren.greg@0249d32c-82a8-11dd-9299-37fd5bf03096>
2008-11-15 21:11:35 +0000

    test score finder_sql

--- test_machine.rb
+++ test_machine.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'test/unit'
+require 'database'
+require 'score'
+require 'player'
+require 'machine'
 
 class TestMachine < Test::Unit::TestCase
   def test_is_grudge_match_machine
@@ -9,5 +13,14 @@ class TestMachine < Test::Unit::TestCase
     GrudgeMatch.add_score("js","ps","Galaga")
     assert(Machine.find_by_name("Galaga").is_grudge_match_machine?)
     Database.disconnect
+  end
+  
+  def test_scores_only_gets_highest_per_machine
+    Database.test_connect
+    Score.add_high_score("AA", "M1", 100)
+    assert_equal(1, Machine.find_by_name("M1").score.size())
+    Score.add_high_score("AA", "M2", 200)
+    Score.add_high_score("AA", "M1", 300)
+    assert_equal(2, Machine.find_by_name("M1").score.size())
   end
 end
