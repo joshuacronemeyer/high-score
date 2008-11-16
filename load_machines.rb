fd4676998a131016b8529ef398bef80a1e45f3c7
joshuacronemeyer <joshuacronemeyer@0249d32c-82a8-11dd-9299-37fd5bf03096>
2008-11-16 07:41:09 +0000

    game list changed

--- load_machines.rb
+++ load_machines.rb
@@ -1,7 +1,7 @@
 require 'database'
 require 'machine'
 Database.test_connect
-list_of_machines = ["WHO DUNNIT","ROLLING THUNDER","NO FEAR","ARKANOID","HYPERBALL","RAMPAGE","ROAD SHOW","STAR TREK","DONKEY KONG JR","MR DO","MS PACMAN","SARGE","DOCTOR WHO","HOME RUN","QIX","ROOT BEER TAPPER","FROGGER","TEMPEST","SATANS HOLLOW","BOMB JACK","APB","GALAGA","DONKEY KONG","BURGER TIME","SPACE INVADERS","I ROBOT","MANIA CHALLENGE","DR WHO","MAPPY"]
+list_of_machines =["Arkanoid","Tempest","Mr. Do","Ms. Pac","APB","Tapper","I, Robot","Bomb Jack","Rampage (1 credit)","Satan's Hollow","Ridge Racer (time trial)","Mappy","Pengo","Sarge","Frogger","Ultra Hockey","Strike Zone Baseball (3 innings)","Gridiron Fight","Mania Challenge (best of 3)","All American Football (1 quarter or 1 half)","High Impact Football (1 half)","Relief Pitcher (may be replaced by Snow Bros)","The Grid (best of 3)","Star Trek","Dr. Who","Big Bang Bar","No Fear","Hyperball","Home Run","Guns and Roses"]
 list_of_machines.each do |machine|
   Machine.new(:name => machine).save
 end
