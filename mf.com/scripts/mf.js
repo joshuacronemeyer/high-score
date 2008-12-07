45f2dc92c9115e92dbbd0fd1b7c10947cc06bf2a
warren.greg <warren.greg@0249d32c-82a8-11dd-9299-37fd5bf03096>
2008-12-07 21:50:32 +0000

    added postion to floating box

--- mf.com/scripts/mf.js
+++ mf.com/scripts/mf.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("#mfFloater").dialog({
-        autoOpen:true,
+        autoOpen:false,
         height:160,
         width:140,
         dialogClass:"floatBox",
@@ -9,24 +9,25 @@ $(document).ready(function() {
     });
     $(".pool").hover(
         function () {
-            show_float("images/pool/pool_ls.jpg", "Look, a pool");
+            show_float("images/pool/pool_ls.jpg", "Look, a pool","250px","260px");
         },
         function() {
             close_float();
         });
     $(".blockhouse").hover(
         function () {
-            show_float("images/blockhouse/gym1.jpg", "Concrete Blockhouse");
+            show_float("images/blockhouse/gym1.jpg", "Concrete Blockhouse","360px","400px");
         },
         function() {
             close_float();
         });
 });
-function show_float(image, title) {
+function show_float(image, title, top, left) {
     $("#mfFloater").dialog("open");
     $("#floaterPic").attr({src: image});
     $("#ui-dialog-title-mfFloater").text(title);
-
+    $(".ui-dialog").css("top",top);
+    $(".ui-dialog").css("left",left);
 }
 
 function close_float() {
